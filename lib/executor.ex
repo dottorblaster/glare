080454c05cfef7da48f8a1276f3ba5b681f2a92f
Alessio Biancalana <dottorblaster@gmail.com>
2018-12-23 20:14:20 +0100

    feat: implement Siren GenServer executor

--- lib/executor.ex
+++ lib/executor.ex
@@ -2,11 +2,12 @@ defmodule Glare.Executor do
     use GenServer
     
     def start_link(_) do
-        GenServer.start_link(__MODULE__, %{})
+        GenServer.start_link(__MODULE__, %{}, name: :executor)
     end
 
     def init(state) do
-        schedule_execution()
+        # This is for when we'll implement websocket.
+        # schedule_execution()
         {:ok, state}
     end
 
@@ -14,6 +15,11 @@ defmodule Glare.Executor do
         Glare.TasksChannel.broadcast(%{tasks: "topkek"})
         schedule_execution()
         {:noreply, state}
+    end
+
+    def handle_call(:tasks, _from, state) do
+        command_output = Glare.SirenWrapper.run_siren()
+        {:reply, command_output, state}
     end
 
     defp schedule_execution() do
