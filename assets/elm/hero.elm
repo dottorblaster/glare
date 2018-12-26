18ac5f3f4932a96ce509e11e0d941d08c39e81d5
Alessio Biancalana <dottorblaster@gmail.com>
2018-12-26 17:34:47 +0100

    feat: refactor types and finalize frontend

--- assets/elm/hero.elm
+++ assets/elm/hero.elm
@@ -4,15 +4,18 @@ import Bulma.Elements exposing (..)
 import Bulma.Layout exposing (..)
 import Bulma.Modifiers exposing (..)
 import Html exposing (Html, text)
+import List exposing (length)
+import String exposing (fromInt, join)
+import Types exposing (Tasks)
 
 
-glareHero : Html msg
-glareHero =
+glareHero : Tasks -> Html msg
+glareHero tasks =
     hero { heroModifiers | size = Small, color = Light, bold = True }
         []
         [ heroBody []
             [ container []
-                [ title H1 [] [ text "Hero Title" ]
+                [ title H1 [] [ text (join " " [ length tasks |> fromInt, "tasks" ]) ]
                 , title H3 [] [ text "Here they are, in all their beauty" ]
                 ]
             ]
--- assets/elm/taskcard.elm
+++ assets/elm/taskcard.elm
@@ -2,15 +2,50 @@ module TaskCard exposing (taskCard)
 
 import Bulma.Columns exposing (..)
 import Bulma.Components exposing (..)
-import Html exposing (Html, main_, text)
+import Bulma.Elements exposing (..)
+import Bulma.Modifiers exposing (..)
+import Bulma.Modifiers.Typography exposing (Weight(..), italicize, textSize, textWeight)
+import Html exposing (Html, code, div, main_, text)
 import Html.Attributes exposing (class)
+import Types exposing (Task)
 
 
-taskCard : String -> Html msg
-taskCard internalText =
+taskBadge : String -> Html msg
+taskBadge exit_code =
+    case exit_code of
+        "0" ->
+            tag { tagModifiers | size = Medium, color = Success }
+                []
+                [ text "OK"
+                ]
+
+        _ ->
+            tag { tagModifiers | size = Medium, color = Danger }
+                []
+                [ text "KO"
+                ]
+
+
+taskCard : Task -> Html msg
+taskCard task =
     column columnModifiers
         [ class "is-half glare-card-column" ]
         [ card [ class "glare-card-el" ]
-            [ cardContent [] [ text internalText ]
+            [ cardContent []
+                [ div [ class "glare-task-name", textWeight Bold ]
+                    [ text task.name
+                    ]
+                , div [ class "glare-task-description", italicize, textSize Bulma.Modifiers.Typography.Small ]
+                    [ text task.description
+                    ]
+                , div [ class "glare-task-command", textSize Bulma.Modifiers.Typography.Small ]
+                    [ code []
+                        [ text task.command
+                        ]
+                    ]
+                , div [ class "glare-task-badge" ]
+                    [ taskBadge task.code
+                    ]
+                ]
             ]
         ]
