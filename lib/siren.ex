a8fb9e859ca99073032d15a90a715ecceb58a157
Alessio Biancalana <dottorblaster@gmail.com>
2018-12-28 21:19:00 +0100

    feat: comply with distillery spec for variables

--- lib/siren.ex
+++ lib/siren.ex
@@ -4,10 +4,11 @@ defmodule Glare.SirenWrapper do
   """
 
   @command "siren"
-  @sirenfile_path System.get_env("SIRENFILE_PATH")
 
   def run_siren() do
-    {output, _exit_code} = System.cmd(@command, ["--file", @sirenfile_path, "--json-output"])
+    {output, _exit_code} =
+      System.cmd(@command, ["--file", System.get_env("SIRENFILE_PATH"), "--json-output"])
+
     Jason.decode!(output)
   end
 end
