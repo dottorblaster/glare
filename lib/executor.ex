4e4466f20331d6c37c50a7c98b07b3643eb29330
Alessio Biancalana <dottorblaster@gmail.com>
2019-12-28 16:37:12 +0100

    Update last execution time inside executor state

--- lib/executor.ex
+++ lib/executor.ex
@@ -5,9 +5,14 @@ defmodule Glare.Executor do
   @moduledoc """
   A GenServer that executes Siren.
   """
+  
+  @initial_state %{
+    "tasks" => [],
+    "last_execution" => DateTime.utc_now() |> DateTime.to_string 
+  }
 
   def start_link(_) do
-    GenServer.start_link(__MODULE__, %{"tasks" => []}, name: :executor)
+    GenServer.start_link(__MODULE__, @initial_state, name: :executor)
   end
 
   def init(state) do
@@ -15,11 +20,18 @@ defmodule Glare.Executor do
     {:ok, state}
   end
 
-  def handle_info(:work, state) do
+  def handle_info(:work, _state) do
     %{"tasks" => task_list} = Glare.SirenWrapper.run_siren()
     schedule_execution(30)
+
     Logger.info("Tasks list refreshed successfully")
-    {:noreply, %{"tasks" => task_list}}
+
+    new_state = %{
+      "tasks" => task_list,
+      "last_execution" => string_now()
+    }
+
+    {:noreply, new_state}
   end
 
   def handle_call(:tasks, _from, state) do
@@ -29,4 +41,6 @@ defmodule Glare.Executor do
   defp schedule_execution(seconds) do
     Process.send_after(self(), :work, seconds * 1000)
   end
+
+  defp string_now, do: DateTime.utc_now |> DateTime.to_string
 end
