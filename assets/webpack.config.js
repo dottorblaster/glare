94d41082081bd958659f24feb0b23ac543f83f09
Alessio Biancalana <dottorblaster@gmail.com>
2021-01-29 11:25:36 +0100

    Upgrade copywebpackplugin config

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,40 +1,38 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-const outputDir = path.join(__dirname, '../priv/static/js');
-const isProd = process.env.NODE_ENV === 'production';
+const outputDir = path.join(__dirname, "../priv/static/js");
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   optimization: {
-    minimizer: [
-      new OptimizeCSSAssetsPlugin({}),
-    ],
+    minimizer: [new OptimizeCSSAssetsPlugin({})],
   },
-  entry: './src/Index.bs.js',
-  mode: isProd ? 'production' : 'development',
+  entry: "./src/Index.bs.js",
+  mode: isProd ? "production" : "development",
   output: {
     path: outputDir,
-    filename: 'app.js'
+    filename: "app.js",
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-    new CopyWebpackPlugin([{ from: 'static/', to: '../' }]),
+    new MiniCssExtractPlugin({ filename: "../css/app.css" }),
+    new CopyWebpackPlugin({ patterns: [{ from: "static/", to: "../" }] }),
   ],
   devServer: {
     compress: true,
     contentBase: outputDir,
     port: process.env.PORT || 8000,
-    historyApiFallback: true
-  }
+    historyApiFallback: true,
+  },
 };
